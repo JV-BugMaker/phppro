5b97d7613553de1bd65297e685426e93d5fa973a
JV-BugMaker <794241063@qq.com>
2016-09-15 16:02:01 +0800

    [*]修改框架中日志类

--- framework/core/lib/driver/log/file.class.php
+++ framework/core/lib/driver/log/file.class.php
@@ -15,16 +15,18 @@ class File
     public $path ;
     public function __construct()
     {
+        //初始化就加载配置文件
         $this->path = Config::get('OPTIONS','log')['PATH'];
     }
 
     public function log($message,$file='log'){
-        //获取配置参数
+        //获取配置参数  进行文件夹创建问题
         if(!is_dir($this->path)){
             mkdir($this->path,'0777',true);
         }
         $date = date('Y-m-d H:i:s',time());
         $log = "[log {$date}]{$message}";
+        //设置log目录用户 用户组 为nobody
         file_put_contents($this->path.$file.'.log',$log.PHP_EOL,FILE_APPEND);
     }
 }
